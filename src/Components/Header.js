10adaf58db0eb3cbe7f5c1cc7627d575e2de87fe
Justin Alvarez <justinalvarez.1221@gmail.com>
2022-12-11 12:15:49 -0600

    testing

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import { getDefaultNormalizer } from "@testing-library/react"
+
+const Header = () => {
+    return(
+        <h1>
+            TESTING JAB 
+        </h1>
+    )
+}
+
+export default Header
